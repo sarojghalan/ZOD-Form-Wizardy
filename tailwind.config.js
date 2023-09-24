dd797b7eff2e213f68864d9aed2c013b08cc4285
Hypekage <jmestmnag@gmail.com>
2023-09-24 20:42:18 +0545

    ` colors ` variants created

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#304766",
+      secondary: "#F38218",
+      tertiary: "#FAFAFA",
+      offWhite: "#D5D8DC",
+      error: "#DC2626",
+    },
+  },
+  plugins: [],
+};
+
