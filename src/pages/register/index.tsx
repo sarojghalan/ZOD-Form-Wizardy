f92311ec6ea3512c249ac0115b5a4877d905c02c
Hypekage <jmestmnag@gmail.com>
2023-09-25 22:34:42 +0545

    `FormProvider` & `FormContext` implemented

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -3,15 +3,22 @@ import PersonalInformation from "./components/tabs/personal-information";
 import Contact from "./components/tabs/contact";
 import Address from "./components/tabs/address";
 import Relationship from "./components/tabs/relationship";
-
+import { FormProvider } from "react-hook-form";
+import { useZodForm } from "../../hooks/requestMethod/useZodForm";
+import { REGISTER_DEFAULT_VALUES } from "./constant";
+import { registerSchema, RegisterSchemaInput, RegisterSchemaOutput } from "./schema";
 const Register = () => {
   const [tabValue, setTabValue] = React.useState<number>(1);
   const tabValues: number[] = [1, 2, 3, 4];
-
+  const methods = useZodForm<RegisterSchemaInput>({
+    schema: registerSchema,
+    defaultValues: REGISTER_DEFAULT_VALUES,
+    mode: "onChange",
+  });
   const Tabs = () => (
     <ul className="flex gap-5 items-center justify-center">
       {tabValues.map((item) => (
-        <li onClick={() => setTabValue(item)}>
+        <li onClick={() => setTabValue(item)} key={item}>
           <span
             className={`${
               tabValue === item
@@ -26,6 +33,10 @@ const Register = () => {
     </ul>
   );
 
+  const submitHandler = (data: RegisterSchemaInput) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className="bg-primary py-6 fixed top-0 left-0 w-full ">
@@ -37,10 +48,16 @@ const Register = () => {
         </div>
       </div>
       <div className="p-4 mt-32">
-        {tabValue === 1 && <PersonalInformation setTabValue={setTabValue} />}
-        {tabValue === 2 && <Contact setTabValue={setTabValue} />}
-        {tabValue === 3 && <Address setTabValue={setTabValue} />}
-        {tabValue === 4 && <Relationship setTabValue={setTabValue} />}
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(submitHandler)}>
+            {tabValue === 1 && (
+              <PersonalInformation setTabValue={setTabValue} />
+            )}
+            {/* {tabValue === 2 && <Contact setTabValue={setTabValue} />}
+            {tabValue === 3 && <Address setTabValue={setTabValue} />}
+            {tabValue === 4 && <Relationship setTabValue={setTabValue} />} */}
+          </form>
+        </FormProvider>
       </div>
     </>
   );
