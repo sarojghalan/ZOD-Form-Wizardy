9b3d51da2a7ca60a18faee7e1b637412bd26ad3e
Hypekage <jmestmnag@gmail.com>
2023-09-25 22:30:11 +0545

    ` type checking ` revamped.

--- src/components/InputFieldText/index.tsx
+++ src/components/InputFieldText/index.tsx
@@ -1,43 +1,65 @@
-import { CommonI, DistrictI, PalikaI } from "../../constants";
-interface InputFieldTextProps {
-  labelText: string;
-  inputType: string;
-  placeHolderText: string;
-  required?: boolean;
-  arr?: CommonI[] | DistrictI[] | PalikaI[];
+import { ComponentProps, forwardRef, useId } from "react";
+import { Slot } from "@radix-ui/react-slot";
+
+interface InputTextFieldProps extends Omit<ComponentProps<"input">, "ref"> {
+  asChild?: boolean;
+  required?:boolean;
+  label?: string;
+  labelProps?: ComponentProps<"label">;
 }
-export const InputFieldText = ({
-  labelText,
-  inputType,
-  placeHolderText,
-  required = false,
-  arr,
-}: InputFieldTextProps) => {
+export const InputTextField = forwardRef(
+  (
+    {
+      asChild,
+      label,
+      required,
+      labelProps: rawLabelProps = {},
+      ...inputProps
+    }: InputTextFieldProps,
+    ref
+  ) => {
+    const Comp = asChild ? Slot : "input";
+
+    const { className, ...labelProps } = rawLabelProps;
+    const id = useId();
+
+    return (
+      <div>
+        <label
+          htmlFor={id}
+          className={"font-medium" + className}
+          {...labelProps}
+        >
+          {label} {required ? <span className="text-error">*</span> : "(optional)"}
+        </label>
+        {/* <br /> */}
+        <Comp
+          id={id}
+          className="w-full rounded-md focus:outline-none border-2 py-2 px-3 my-3 border-offWhite"
+          {...inputProps}
+          ref={ref as never}
+        />
+      </div>
+    );
+  }
+);
+
+export const MainInput = () => {
   return (
     <div>
-      <label htmlFor="" className="font-medium ">
-        {labelText}{" "}
-        {required ? (
-          <span className="text-error font-medium">*</span>
-        ) : (
-          "(optional)"
-        )}{" "}
-      </label>
-      <br />
-      {inputType === "select" ? (
-        <select className="w-full rounded-md focus:outline-none border-2 py-2 px-3 my-3 border-offWhite">
-          <option value="">Select {labelText}</option>
-          {arr?.map((item) => (
-            <option key={item?.id}>{item?.name}</option>
-          ))}
+      <InputTextField placeholder="Hello"/>
+      <InputTextField asChild>
+        <textarea />
+      </InputTextField>
+      <InputTextField  asChild>
+        <select className="bg-secondary">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
         </select>
-      ) : (
-        <input
-          className="w-full rounded-md focus:outline-none border-2 py-2 px-3 my-3 border-offWhite"
-          type={inputType}
-          placeholder={placeHolderText}
-        />
-      )}
+      </InputTextField>
     </div>
   );
 };
+
