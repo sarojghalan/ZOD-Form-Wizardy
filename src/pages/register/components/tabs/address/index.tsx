78892fa7e83bf74d0fdf062baf66fdbfcdcf1e79
Hypekage <jmestmnag@gmail.com>
2023-09-25 22:31:24 +0545

    updated ` inputTextField ` component used

--- src/pages/register/components/tabs/address/index.tsx
+++ src/pages/register/components/tabs/address/index.tsx
@@ -3,59 +3,64 @@ import {
   DISTRICT_DATA,
   PALIKA_DATA,
   PROVINCE_DATA,
+  CommonI,
+  PalikaI,
+  DistrictI,
 } from "../../../../../constants";
 import CommonButton from "../../../../../components/button/commonButton";
-import { InputFieldText } from "../../../../../components/InputFieldText";
+import { useForm } from "react-hook-form";
+import { AddressArrSchema, AddressArrSchemaExtractInput } from "../../../schema";
+import { InputTextField } from "../../../../../components/InputFieldText";
 interface AddressProps {
   setTabValue: (data: number) => void;
 }
 
 const Address = ({ setTabValue }: AddressProps) => {
-  const nextHandler = () => {
-    setTabValue(4);
-  };
-  const prevHandler = () => {
-    setTabValue(2);
-  };
   return (
     <div>
       <h2>Address Information</h2>
       <h3>Permanent Address</h3>
-      <InputFieldText
+      <InputTextField
         labelText="State"
         inputType="select"
         placeHolderText="Enter Your State ..."
         required
         arr={PROVINCE_DATA}
+        value="sCode"
       />
-      <InputFieldText
+      <InputTextField
         labelText="District"
         inputType="select"
         placeHolderText="Enter Your District ..."
         required
         arr={DISTRICT_DATA[0].data}
+        value="dCode"
       />
-      <InputFieldText
+      <InputTextField
         labelText="Palika"
         inputType="select"
         placeHolderText="Enter Your Palika ..."
         required
         arr={PALIKA_DATA[0].data}
+        value="pCode"
       />
-      <InputFieldText
+      <InputTextField
         labelText="City"
         inputType="text"
         placeHolderText="Enter Your City ..."
         required
+        value="city"
       />
-      <InputFieldText
+      <InputTextField
         labelText="Street "
+        value="street"
         inputType="text"
         placeHolderText="Enter Your Street ..."
         required
       />
-      <InputFieldText
+      <InputTextField
         labelText="Contact No."
+        value="contactNo"
         inputType="number"
         placeHolderText="Enter Your Contact No ..."
         required
@@ -64,53 +69,46 @@ const Address = ({ setTabValue }: AddressProps) => {
         <h3>Temporary Address</h3>
         <h3 className="underline cursor-pointer">Set as permanent</h3>
       </div>
-      <InputFieldText
+      <InputTextField
         labelText="State"
         inputType="select"
         placeHolderText="Enter Your State ..."
         required
         arr={PROVINCE_DATA}
       />
-      <InputFieldText
+      <InputTextField
         labelText="District"
         inputType="select"
         placeHolderText="Enter Your District ..."
         required
         arr={DISTRICT_DATA[0].data}
       />
-      <InputFieldText
+      <InputTextField
         labelText="Palika"
         inputType="select"
         placeHolderText="Enter Your Palika ..."
         required
         arr={PALIKA_DATA[0].data}
       />
-      <InputFieldText
+      <InputTextField
         labelText="City"
         inputType="text"
         placeHolderText="Enter Your City ..."
         required
       />
-      <InputFieldText
+      <InputTextField
         labelText="Street "
         inputType="text"
         placeHolderText="Enter Your Street ..."
         required
       />
-      <InputFieldText
+      <InputTextField
         labelText="Contact No."
         inputType="number"
         placeHolderText="Enter Your Contact No ..."
         required
       />
-      <CommonButton name="Next" handler={nextHandler} />
-      <CommonButton
-        name="Back"
-        handler={prevHandler}
-        backgroundColor="bg-tertiary"
-        textColor="text-secondary"
-        borderColor="border-secondary"
-      />
+      <CommonButton name="Submit" handler={handleSubmit(submitHandler)} />
     </div>
   );
 };
