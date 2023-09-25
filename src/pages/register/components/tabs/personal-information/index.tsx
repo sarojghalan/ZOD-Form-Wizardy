39a06bc8fa9e679f63fc9ebbfd7d69c79a28737f
Hypekage <jmestmnag@gmail.com>
2023-09-25 22:32:15 +0545

    `Slot` component implemented.

--- src/pages/register/components/tabs/personal-information/index.tsx
+++ src/pages/register/components/tabs/personal-information/index.tsx
@@ -4,68 +4,106 @@ import {
   MARITAL_STATUS_DATA,
   BLOOD_GROUP_DATA,
 } from "../../../../../constants";
-import { InputFieldText } from "../../../../../components/InputFieldText";
-
+import { InputTextField } from "../../../../../components/InputFieldText";
+import { useFormContext } from "react-hook-form";
+import React from "react";
+import { RegisterSchemaInput } from "../../../schema";
 interface PersonalInformationProps {
   setTabValue: (value: number) => void;
 }
 
 const PersonalInformation = ({ setTabValue }: PersonalInformationProps) => {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors, isValidating, isDirty },
+  } = useFormContext<RegisterSchemaInput>();
+
   const nextHandler = () => {
     setTabValue(2);
   };
+
+  const submitHandler = () => {
+    console.log(getValues("firstName"));
+    console.log("hahah");
+  };
+
   return (
     <div>
       <h2>Personal Information</h2>
-      <InputFieldText
-        labelText="First Name"
-        inputType="text"
-        placeHolderText="Enter Your First Name ..."
+      <InputTextField
+        label="First Name"
+        type="text"
+        placeholder="Enter Your First Name ..."
         required
+        {...register("firstName")}
       />
-      <InputFieldText
-        labelText="Middle Name"
-        inputType="text"
-        placeHolderText="Enter Your Middle Name ..."
+      <InputTextField
+        label="Middle Name"
+        type="text"
+        placeholder="Enter Your Middle Name ..."
+        {...register("middleName")}
       />
-      <InputFieldText
-        labelText="Last Name"
-        inputType="text"
-        placeHolderText="Enter Your Last Name ..."
+      <InputTextField
+        label="Last Name"
+        type="text"
+        placeholder="Enter Your Last Name ..."
         required
+        {...register("lastName")}
       />
-      <InputFieldText
-        labelText="Gender"
-        inputType="select"
-        placeHolderText="Enter Your Gender ..."
+      <InputTextField
+        label="Gender"
+        placeholder="select"
+        {...register("genderId")}
         required
-        arr={GENDER_DATA}
+        asChild
+      >
+        <select>
+          {GENDER_DATA.map((item) => (
+            <option value={item.id}>{item.name}</option>
+          ))}
+        </select>
+      </InputTextField>
+      <InputTextField
+        label="Marital Status"
+        placeholder="select"
+        {...register("mStatusId")}
+        required
+        asChild
+      >
+        <select>
+          {MARITAL_STATUS_DATA.map((item) => (
+            <option value={item.id}>{item.name}</option>
+          ))}
+        </select>
+      </InputTextField>
+      <InputTextField
+        label="Blood Group"
+        placeholder="select"
+        {...register("bloodGrpId")}
+        required
+        asChild
+      >
+        <select>
+          {BLOOD_GROUP_DATA.map((item) => (
+            <option value={item.id}>{item.name}</option>
+          ))}
+        </select>
+      </InputTextField>
+      <InputTextField
+        label="Date od Birth"
+        type="date"
+        placeholder="Enter Your Last Name ..."
+        required
+        {...register("dateOfBirth")}
       />
-      <InputFieldText
-        labelText="Marital Status"
-        inputType="select"
-        placeHolderText="Enter Your Marital Status ..."
+      <InputTextField
+        label="National Id"
+        type="text"
+        placeholder="Enter Your Last Name ..."
         required
-        arr={MARITAL_STATUS_DATA}
-      />
-      <InputFieldText
-        labelText="Blood Group"
-        inputType="select"
-        placeHolderText="Enter Your Blood Group ..."
-        required
-        arr={BLOOD_GROUP_DATA}
-      />
-      <InputFieldText
-        labelText="Date of BIrth"
-        inputType="date"
-        placeHolderText="Enter Your D.O.B ..."
-        required
-      />
-      <InputFieldText
-        labelText="National ID"
-        inputType="number"
-        placeHolderText="Enter Your National ID ..."
-        required
+        {...register("nationalId")}
       />
       <CommonButton name="Next" handler={nextHandler} />
       {/* <CommonButton
