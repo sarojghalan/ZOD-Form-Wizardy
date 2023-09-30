1cb2cbad45857e5e6829198b1f8049c0447b0da3
Hypekage <jmestmnag@gmail.com>
2023-09-30 23:27:29 +0545

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { MainInput } from "./components/InputFieldText";
 import Register from "./pages/register";
+import { TextField } from "./components/InputFieldText/textField";
 
 const App = () => {
   return (
-    <Register />
+    <>
+    <TextField  type="text" placeholder="Enter your hobby" />
+    <TextField asChild  type="submit">
+      <button >
+        click me
+      </button>
+    </TextField>
+    <TextField asChild  >
+      <select >
+        <option value="">asdfasdf</option>
+        <option value="">asdfasdf</option>
+        <option value="">asdfasdf</option>
+        <option value="">asdfasdf</option>
+      </select>
+    </TextField></>
   )
 }
 
